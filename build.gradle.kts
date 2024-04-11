070dad53325da6d41b8c0e3151b012a4c2181fc6
Tryfle <111710533+Tryflle@users.noreply.github.com>
2024-04-11 13:23:58 -0700

    I committed the wrong gradle file somehow.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,14 +4,14 @@ plugins {
 }
 
 group = "me.tryfle"
-version = "1.0"
+version = "1.0.1"
 
 weave {
     configure {
-        name = "njd"
-        modId = "njd"
-        entryPoints = listOf("me.tryfle.njd.Main")
-        hooks = listOf("me.tryfle.njd.hook.EntityLivingBaseHook", "me.tryfle.njd.hook.InitEventHook", "me.tryfle.njd.hook.ChatEventHook")
+        name = "LegacyEvents"
+        modId = "legacyevents"
+        entryPoints = listOf("me.tryfle.legacyevents.Main")
+        mixinConfigs = listOf("legacyevents.mixins.json")
         mcpMappings()
     }
     version("1.8.9")
@@ -19,14 +19,17 @@ weave {
 
 repositories {
     mavenCentral()
+    maven("https://repo.spongepowered.org/maven")
     maven("https://repo.weavemc.dev/releases")
 }
 
 dependencies {
     implementation("net.weavemc.api:common:1.0.0-PRE2")
     implementation("net.weavemc:internals:1.0.0-PRE2")
+    compileOnly("org.spongepowered:mixin:0.8.5")
 }
 
 kotlin {
     jvmToolchain(17)
 }
+
